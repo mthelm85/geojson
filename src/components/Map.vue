59dea01afd1a0923eb379221ca19741c61c3eb2f
Matt Helm <mthelm85@gmail.com>
2018-12-17 16:52:32 -0600

    added getColor function, only style counties w/ data

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -9,25 +9,57 @@
 </template>
 
 <script>
+import axios from 'axios'
 import L from 'leaflet'
 const counties = require('@/assets/counties.json')
 export default {
   data () {
     return {
-
+      employmentTotals: []
     }
   },
 
-  mounted () {
-    const getColor = (val) => {
-      return val > 0.8 ? '#f0f9e8'
-        : val > 0.6 ? '#bae4bc'
-          : val > 0.4 ? '#7bccc4'
-            : val > 0.2 ? '#43a2ca'
-              : '#0868ac'
+  methods: {
+    getColor (val, employmentTotals) {
+      const max = Math.max(...employmentTotals)
+      return val > 0.8 * max ? '#0868ac'
+        : val > 0.6 * max ? '#43a2ca'
+          : val > 0.4 * max ? '#7bccc4'
+            : val > 0.2 * max ? '#bae4bc'
+              : '#f0f9e8'
+    },
+    getTotalEmployment (array, val) {
+      return array.find(x => x[10] === val)
     }
-    for (let i in counties.features) {
-      counties.features[i].properties.concentration = Math.random()
+  },
+
+  async mounted () {
+    try {
+      const res = await axios.get('https://api.census.gov/data/timeseries/qwi/sa', {
+        params: {
+          get: 'Emp',
+          for: 'county:*',
+          in: `state:06`,
+          year: '2017',
+          quarter: '3',
+          sex: '0',
+          agegrp: 'A01',
+          ownercode: 'A05',
+          firmsize: '0',
+          seasonadj: 'U',
+          industry: '11',
+          key: 'f0c0e4dc0941d95473e9ce88d697d682b1d7fca4'
+        }
+      })
+      // Store just the employment totals into a new array so that we can compute max in getColor (above)
+      this.employmentTotals = res.data.map(element => element[0]).filter(num => num > 0)
+      for (let i in counties.features) {
+        if (counties.features[i].properties.STATE === '06' && this.getTotalEmployment(res.data, counties.features[i].properties.COUNTY) !== undefined) {
+          counties.features[i].properties.totalEmployment = this.getTotalEmployment(res.data, counties.features[i].properties.COUNTY)[0]
+        }
+      }
+    } catch (err) {
+      console.log(err)
     }
     const map = L.map('map', { zoomControl: true })
       .locate({ setView: true, maxZoom: 6 })
@@ -38,12 +70,30 @@ export default {
       minZoom: 4
     }).addTo(map)
     L.geoJSON(counties, {
-      style (feature) {
-        return {
-          'color': getColor(feature.properties.concentration),
-          'weight': 0.1,
-          'opacity': 1,
-          'fillOpacity': 0.5
+      style: (feature) => {
+        if (feature.properties.totalEmployment && feature.properties.totalEmployment !== null) {
+          return {
+            'color': this.getColor(feature.properties.totalEmployment, this.employmentTotals),
+            'weight': 0.5,
+            'opacity': 1,
+            'fillOpacity': 0.5
+          }
+        } else {
+          return {
+            'color': null,
+            'weight': 0.5,
+            'opacity': 1,
+            'fillOpacity': 0
+          }
+        }
+      },
+      onEachFeature: (feature, layer) => {
+        if (feature.properties.totalEmployment && feature.properties.totalEmployment !== null) {
+          layer.bindPopup(`
+              <strong>County:</strong> ${feature.properties.NAME}
+              <br />
+              <strong>Total Employment:</strong> ${feature.properties.totalEmployment}
+            `)
         }
       }
     }).addTo(map)
